f1a615bcc32065b9b78e8e0d586ae7963536e637
realScipio <33896151+realScipio@users.noreply.github.com>
2018-07-16 17:29:00 +0200

    Added main.py, flask.cfg, changed to app.config.from_pyfile(config_file)

---
+++ main.py
@@ -0,0 +1,3 @@
+from scippycrm import create_app
+
+app = create_app('flask.cfg')
--- scippycrm/__init__.py
+++ scippycrm/__init__.py
@@ -7,14 +7,17 @@ from wtforms.validators import DataRequired
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
-def create_app(test_config=None):    
+def create_app(config_file=None):    
     app = Flask(__name__, instance_relative_config=True)
+    app.config.from_pyfile(config_file)
+    '''
     app.config.from_mapping(
         SECRET_KEY = 'Very_very_secret!',
         USERNAME = 'admin',
         PASSWORD = 'admin',
         MONGO_URI = 'mongodb://localhost:27017/scippycrm'
     )
+    '''
     mongo = PyMongo(app)
 
     def login_required(f):
